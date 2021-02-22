def49d8718372fae91005aecc0b83dff865551e1
Kantinun <s6201012630011@email.kmutnb.ac.th>
2021-02-22 16:37:34 +0700

    change url name

--- mysite/vocab/urls.py
+++ mysite/vocab/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'vocab'
 urlpatterns = [
-    path('', views.homePage, name='index'),
+    path('', views.homePage, name='homepage'),
     path('main', views.index, name='index'),
     path('<int:vocab_id>/', views.detail , name='detail'),
     path('addPage/', views.addWordPage , name='addPage'),

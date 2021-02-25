57358c4fd3dc9a726a0b2323b93c9b95075458dc
Kantinun <s6201012630011@email.kmutnb.ac.th>
2021-02-25 12:24:14 +0700

    change path and add poll

--- mysite/vocab/urls.py
+++ mysite/vocab/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'vocab'
 urlpatterns = [
     path('', views.homePage, name='homepage'),
-    path('main', views.index, name='index'),
-    path('<int:vocab_id>/', views.detail , name='detail'),
+    path('main/', views.index, name='index'),
+    path('main/<int:vocab_id>/', views.detail , name='detail'),
     path('addPage/', views.addWordPage , name='addPage'),
     path('add/', views.addWord , name='add'),
 ]
